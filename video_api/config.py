3862bea9735adcb8152e264902293f5d223f3583
notness <inessa.fedorova.01@mail.ru>
2023-09-07 21:57:58 +0300

    initial commit for folder for image synthesis

---
+++ video_api/config.py
@@ -0,0 +1,9 @@
+# model info
+model_id = "nitrosocke/mo-di-diffusion"
+steps = 30
+device = "cpu"
+
+host = '0.0.0.0'
+port = 9875
+media_path = './media'
+
---
+++ video_api/src/api.py
@@ -0,0 +1,47 @@
+import os
+import io
+from typing import List, Union
+
+import librosa
+import soundfile as sf
+from fastapi import status, APIRouter, File
+from starlette.responses import StreamingResponse
+
+import config
+from src.inference import generate_images
+from src.utils import add_audio, create_video, generate_images_demo
+
+router = APIRouter()
+
+
+@router.post("/generate", description='Метод для генерации изображений.')
+async def synthesize(
+        audio: bytes = File(...),
+        sample_rate: int = 16000,
+        animal: str = 'dog',
+        color: str = 'black',
+        mistakes: Union[List, None] = None,
+) -> StreamingResponse:
+    parameters = {
+        'animal': animal,
+        'color': color
+    }
+
+    audio = librosa.load(io.BytesIO(audio), sr=sample_rate)[0]
+    duration = len(audio) / sample_rate
+    audio_path = os.path.join(config.media_path, 'audio.wav')
+
+    sf.write(audio_path, audio.transpose(), sample_rate, 'PCM_16')
+    images = generate_images(parameters)
+
+    mistakes = eval(mistakes)
+    if mistakes:
+        mistakes = [mistake for mistake in mistakes if mistake]
+
+    video_path = create_video(images, mistakes=mistakes, audio_duration=duration, media_path=config.media_path)
+    final_file = add_audio(config.media_path, audio_path, video_path)
+
+    response = StreamingResponse(iter(open(final_file, "rb")), status_code=status.HTTP_200_OK)
+    response.headers["Content-Disposition"] = "attachment; filename={}".format('video.mp4')
+
+    return response
